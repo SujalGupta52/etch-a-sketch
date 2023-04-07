49437cf61fc241ca394fa6c8d35c125fc47dfdc1
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-04-08 01:12:23 +0530

    Refactor script.js and add option to change grid

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
+function initialiseContainer(size) {
 const container = document.createElement('div');
-const mainBox = document.querySelector('.mother-box');
-let n = 64;
 container.classList.add('container');
-console.log(mainBox.offsetHeight);
-for(i = 0; i < n*n; i++) {
+
+for(i = 0; i < (size*size); i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
 
-    pixel.style.height = mainBox.offsetHeight/n + 'px';
-    pixel.style.width = mainBox.offsetWidth/n + 'px';
+    pixel.style.height = mainBox.offsetHeight / size + 'px';
+    pixel.style.width = mainBox.offsetWidth / size + 'px';
 
     pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = 'black';
+        if(color === 'rainbow')
+            pixel.style.backgroundColor = randomRGB();
+        else
+            pixel.style.backgroundColor = color;
     });
     
     container.appendChild(pixel);
 }
-
 document.querySelector('.mother-box').appendChild(container);
+}
+
+function initialiseButton() {
+    document.querySelector('.draw-button').addEventListener('click', () => color = 'black');
+    document.querySelector('.erase-button').addEventListener('click', () => color = 'white');
+    document.querySelector('.rainbow-button').addEventListener('click', () => color = 'rainbow');
+    document.querySelector('.grid-size').addEventListener('click', () => {
+        for(;;) {
+            const size = Number(prompt('Enter grid size(less than 100):','16'));
+            console.log(size);
+            if(size < 100 && size > 0) {
+                mainBox.removeChild(document.querySelector('.container'));
+                initialiseContainer(size);
+                break;
+            }
+            else {
+                alert('Invalid Size!');
+            }
+        }
+    });
+}
+
+function randomRGB() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`; 
+    return rgb;
+}
+
+const mainBox = document.querySelector('.mother-box');
+let color = 'white';
+
+initialiseContainer(16);
+initialiseButton();
+
